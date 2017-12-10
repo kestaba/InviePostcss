ac4022030f23f6d626d1f8509b12a70abe6e67bd
kestaba <kestaba.dev@gmail.com>
2017-12-09 21:19:53 -0400

    Agregadas dependencias para trabajar con gulp y postcss.
    Configuradas propiedades personalizadas.

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp')
+var postcss = require('gulp-postcss')
+var cssnext = require('postcss-cssnext')
+var autoprefixer = require('autoprefixer')
+var cssnested = require('postcss-nested')
+var browserSync = require('browser-sync').create()
+
+
+// Servidor de desarrollo
+
+gulp.task('serve', function(){
+    browserSync.init({
+        server: {
+            baseDir: 'dist' //ruta donde se van a servir los ficheros como la raiz.
+        }
+    })
+})
+
+// Tarea para procesar el CSS
+
+gulp.task('css', function(){
+    var processors = [
+        //autoprefixer({ browsers: ['> 5%', 'ie 8']}), // Plugin de postcss, estos deben llevar un cierto orden para evitar errores.
+                                                    /* 1. A que navegadores queremos dar soporte, ej. más del 5% de utilización, de lo contrario no le colocará esos procesos especiales que tenga para ellos.
+                                                     2. Que de soporte a IE a partir del 8, de esta manera incluirá vendorprefixer para estos navegadores.*/
+        cssnested,
+        cssnext({ browsers: ['> 5%', 'ie 8']}) // Ya incluye autoprefixier
+    ]   
+    return gulp.src('src/css/*.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream())
+})
+
+
+// Tarea de vigilancia
+
+gulp.task('watch', function(){
+    gulp.watch('src/css/*.css', ['css'])
+    gulp.watch('dist/*.html').on('change', browserSync.reload)
+})
+
+// Tarea por defecto
+
+gulp.task('default', ['watch', 'serve'])
